da757cbea2790c95649b5ee362519c5b215da129
Carlos Eduardo <caduadosiqueira@gmail.com>
2023-12-30 03:13:39 -0300

    defining colors

--- src/container/Main.tsx
+++ src/container/Main.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Sidebar from "@/components/Sidebar";
-      
 
 const Main = () => {
+
+
   return (
-    <div className='bg-slate-700 relative w-full'>
+    <div className='bg-neutral-700 relative w-full'>
       <Sidebar></Sidebar>
-      <button className='absolute right-0'>VOLTAR</button>
-</div>
+      <button className='absolute right-0' >VOLTAR</button>
+    </div>
   )
 }
 
